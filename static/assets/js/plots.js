66948b36510f485f75cb730cb90acad1ee86b92f
vishalnarnaware <narnaware_vishal.ghrceai@raisoni.net>
2021-07-09 21:20:22 +0530

    Add Ajax functions for ts

--- static/assets/js/plots.js
+++ static/assets/js/plots.js
@@ -1,11 +1,28 @@
-$('#first_cat').on('change',function(){
+$('#change_data').on('change',function(){
 
     $.ajax({
-        url: "/bar",
+        url: "/data_ts",
         type: "GET",
         contentType: 'application/json;charset=UTF-8',
         data: {
-            'selected': document.getElementById('first_cat').value
+            'selected': document.getElementById('change_data').value
+
+        },
+        dataType:"json",
+        success: function (data) {
+            Plotly.newPlot('bargraph', data );
+        }
+    });
+})
+
+$('#change_type').on('change',function(){
+
+    $.ajax({
+        url: "/type_ts",
+        type: "GET",
+        contentType: 'application/json;charset=UTF-8',
+        data: {
+            'selected': document.getElementById('change_type').value
 
         },
         dataType:"json",
