7a96ebbe699daecf53c505105d1972c91212df11
Shivam Tawari <shivamtawari@gmail.com>
2021-07-24 16:08:07 +0530

    Added Chatbot.js for Hindi Lang

---
+++ static/assets/js/chatbot.js
@@ -0,0 +1,7 @@
+window.__be = window.__be || {};
+window.__be.id = "60f9778c21a44e00079075fb";
+(function() {
+    var be = document.createElement('script'); be.type = 'text/javascript'; be.async = true;
+    be.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'cdn.chatbot.com/widget/plugin.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(be, s);
+})();
